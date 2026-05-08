850b3da39fab2ba28c702512a4dfbb94847d80ea
Evgenii Govorushkin <e.govorushkin@gmail.com>
2026-05-08 22:02:53 +0300

    Initial commit

---
+++ src/main/java/com/egovorushkin/spring_ai/SpringAiTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.egovorushkin.spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiTutorialApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringAiTutorialApplication.class, args);
+    }
+
+
+}
