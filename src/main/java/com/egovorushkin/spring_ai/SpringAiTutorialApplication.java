376ddb67ad538969fa6fd886328cfacc4710ebae
Evgenii Govorushkin <e.govorushkin@gmail.com>
2026-05-08 22:05:06 +0300

    Add chat client functionality with custom memory advisor

--- src/main/java/com/egovorushkin/spring_ai/SpringAiTutorialApplication.java
+++ src/main/java/com/egovorushkin/spring_ai/SpringAiTutorialApplication.java
@@ -1,7 +1,15 @@
 package com.egovorushkin.spring_ai;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClientCustomizer;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringAiTutorialApplication {
@@ -10,5 +18,33 @@ public class SpringAiTutorialApplication {
         SpringApplication.run(SpringAiTutorialApplication.class, args);
     }
 
+    @Bean
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.build();
+    }
+
+    @Bean
+    ApplicationRunner go(ChatClient chatClient) {
+        return _ -> {
+            System.out.println("How can I help?\n");
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    System.out.print("> ");
+                    System.out.println("\n - " +
+                            chatClient.prompt(scanner.nextLine()).call().content());
+                }
+            }
+        };
+    }
+
+    @Bean
+    ChatClientCustomizer chatMemoryCustomizer() {
+        return builder -> builder.defaultAdvisors(
+                MessageChatMemoryAdvisor.builder(
+                                MessageWindowChatMemory.builder()
+                                        .maxMessages(500)
+                                        .build())
+                        .build());
+    }
 
 }
